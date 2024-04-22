77c85fe6043e9af9cf3db2da28069abda81efcfe
Harshvardhan Rijhwani <harshrijhwani4@gmail.com>
2024-04-22 17:32:15 +0530

    7_twoknights done

--- introduction/7_twoknights.cpp
+++ introduction/7_twoknights.cpp
@@ -5,7 +5,9 @@ int main() {
   int n;
   cin >> n;
 
-  for(int i=1;i<=n;i++) {
-    
+  for (int i = 1; i <= n; i++) {
+    int totalCombinations = ((i * i) * ((i * i) - 1)) / 2;
+    int attackingWays = 4 * (i - 1) * (i - 2);
+    cout << totalCombinations - attackingWays << endl;
   }
 }
