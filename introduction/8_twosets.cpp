75443e1393b3136447671cb6ec62a11a81ff427a
Harshvardhan Rijhwani <harshrijhwani4@gmail.com>
2024-04-23 01:06:37 +0530

    8_twosets not done

---
+++ introduction/8_twosets.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+
+ll sum(long n) { return n * (n + 1) / 2; }
+
+int main() {
+  ll n;
+  cin >> n;
+
+  if(sum(n)%2==1){
+    cout<<"NO"<<endl;
+  } else {
+    
+  }
+
+}
